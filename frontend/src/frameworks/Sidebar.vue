77adad75eecf8c3c2fa0104ab3ec1ab50fab9935
Stephen Zeng <stephenzeng@goforit.top>
2025-01-20 23:36:47 +0800

    sideBar Logic improve

--- frontend/src/frameworks/Sidebar.vue
+++ frontend/src/frameworks/Sidebar.vue
@@ -8,6 +8,10 @@
   function sidebarClose() {
     openStatus.value = false;
   }
+  function changeTab(newTab) {
+    tab.value = newTab;
+    sidebarClose();
+  }
 </script>
 
 <template>
@@ -15,11 +19,11 @@
   :open="openStatus" close-on-esc close-on-overlay-click>
     <mdui-list>
       <mdui-list-item icon="task"
-      @click="tab=0">{{ pages[0] }}</mdui-list-item>
+      @click="changeTab(0)">{{ pages[0] }}</mdui-list-item>
       <mdui-list-item icon="miscellaneous_services"
-      @click="tab=1">{{ pages[1] }}</mdui-list-item>
+      @click="changeTab(1)">{{ pages[1] }}</mdui-list-item>
       <mdui-list-item icon="auto_fix_high"
-      @click="tab=2">{{ pages[2] }}</mdui-list-item>
+      @click="changeTab(2)">{{ pages[2] }}</mdui-list-item>
     </mdui-list>
   </mdui-navigation-drawer>
 </template>
